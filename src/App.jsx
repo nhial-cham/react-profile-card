c827dc80ee6b162a370185d5865051856b490fee
Nhial Cham <nhialcham14@gmail.com>
2026-06-01 18:08:27 +0200

    Profile card components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import ProfileCard from './components/ProfileCard.jsx';
+    
 function App() {
     return (
+        <>
         <h1>First react application!</h1>
+        <ProfileCard />
+        </>
     );
 }
+
+export default App;
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,10 @@
+function ProfileCard() {
+    return (
+        <div>
+            <h2>Nhial Cham</h2>
+            <p>Full-Stack Software Developer</p>
+        </div>
+    );
+}
+
+export default ProfileCard;
